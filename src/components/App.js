7180d8fbeece340b098546d846dd75ad603f4af5
Faisal abrar <114504997+Fizz007@users.noreply.github.com>
2022-12-24 15:34:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
-import React , {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import List from "./List";
 const App = () => {
-  //code here
-  
-  const [val,setValue] = useState("");
-  const [list,setList] = useState({});
-  const changehandler = (event)=> {
-    setvalue(event.target.value);
+  const [val, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const changehandler = (event) => {
+    setValue(event.target.value);
   }
   const addhandler = () => {
-    setList([...list,val]);
+    setList([...list, val]);
   }
   return (
     <div id="main">
-    <input id = "input" value={val} onChange={changehandler}/>
-    <button id='button' onClick = {addhandler}>Click</button>
-<ul id='list'>
-  {list.map((element,index)=>{
-    return <li className="items" key = {element + index} > {element} </li>;
-  })}
-  {/* <List listx={list} />*/}
-</ul>
-</div>
-)
+      <input id="input" value={val} onChange={changehandler} />
+      <button id="button" onClick={addhandler}>Click</button>
+      <ul id="list">
+
+        {list.map((element, index) => {
+          return <li className="items" key={element + index} >{element}</li>;
+        })}
+
+        {/* <List listx={list} /> */}
+      </ul>
+    </div>
+  )
 }
 
 
